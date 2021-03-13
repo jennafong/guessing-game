7881c23b83a1d23ee2395e93a0d9b57ce1e0a8e6
Jenna Fong <jfong569@gmail.com>
2021-03-13 20:46:58 +0000

    created exception for initial non int input

--- game.py
+++ game.py
@@ -12,19 +12,32 @@
 #         congratulate player
 
 
+
 from random import randint
 
 player_name = input("What is your name >> ")
 print(f'Hello {player_name}.')
 num = randint(1,100)
-choice = int(input("Choose a number between 1-100 >> "))
+
+while True:
+    try:
+        choice = int(input("Choose a number between 1-100 >> "))
+        if choice > 0:
+            break
+    except ValueError:
+            print("Hahahaha, nice try. Please enter an integer.")
+            
 
 while True:
     if choice != num:
-        if choice > num:
+        if choice >= 100 or choice <= 0:
+            print("Hahahaha, you're funny. Enter a valid number.")
+        elif choice > num:
             print("Your guess is too high.")
         elif choice < num:
             print("Your guess is too low.")
+        
         choice = int(input("Please guess again >> "))
     else:
         print("Congrats!")
+        break
